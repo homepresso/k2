b7452eadd3512359990a6e15b40677da28cd3a1f
homepresso <75093312+homepresso@users.noreply.github.com>
2021-11-09 09:19:50 -0700

    Create removedropshadows.js

---
+++ removedropshadows.js
@@ -0,0 +1,1 @@
+<script>$(".theme-entry .view .innerpanel > .grid, .theme-entry .view .innerpanel > .panel, .theme-entry .view-canvas > .grid, .theme-entry .view-canvas > .panel").css("box-shadow","none")</script>
